652003448d0841f750ee3d00a42afd49acdeddcf
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2024-05-12 16:24:49 +0000

    Added Allowed Host TO sETTINGS

--- tdhc/settings.py
+++ tdhc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+)13q-9205xpy!#3=@=q$07wn95((q7h2!!xyt&tl1d@o1(=!h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
