e3097560e6e16e4489bff74ad4be292c687065f3
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2024-05-29 01:16:32 +0000

    Base Serizlier

--- members/serializers.py
+++ members/serializers.py
@@ -6,16 +6,19 @@ from members.models import Member
 class BaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = None
-        exclude = ["is_deleted"]
-
-
-class MemberSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Member
-        read_only_fields = ["id"]
-        exclude = ["is_deleted"]
+        exclude = [
+            "is_deleted",
+        ]
+        depth = 1
 
     def to_representation(self, instance):
         serializer = BaseSerializer(instance=instance)
         serializer.Meta.model = instance.__class__
         return serializer.data if instance else {}
+
+
+class MemberSerializer(BaseSerializer, serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        read_only_fields = ["id"]
+        exclude = ["is_deleted"]
