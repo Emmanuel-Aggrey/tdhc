f6f25d4989e91942a7d67794436c8421fd270010
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2024-05-29 03:09:18 +0000

    .

--- members/views.py
+++ members/views.py
@@ -46,6 +46,9 @@ class MemberReportView(APIView):
     def get_total_members_baptised(self):
         return Member.objects.exclude(baptismal_date__isnull=True).count()
 
+    def get_total_members_with_occupation(self):
+        return Member.objects.exclude(occupation__isnull=True).count()
+
     def get_marital_status_counts(self):
         return list(Member.objects.values('marital_status').annotate(count=Count('marital_status')))
 
