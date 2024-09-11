8c119aca7f23c68e35495bcc7310a1a1f5e535ef
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2024-09-11 13:11:43 +0000

    .

--- tdhc/settings.py
+++ tdhc/settings.py
@@ -133,7 +133,7 @@ REST_FRAMEWORK = {
     ),
     "EXCEPTION_HANDLER": "core.utils.exception_handler.custom_exception_handler",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 100,
+    "PAGE_SIZE": 15,
 }
 
 SIMPLE_JWT = {
